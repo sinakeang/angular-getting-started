e23c6583c8dff03f784f896e5a5d2e2f2edf5a15
Sina Keang <sinakeang@gmail.com>
2020-01-25 13:17:49 -0500

    Update product-details component with product info based on productId.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
 
-  constructor() { }
+  // ActivatedRoute is specific to each routed component loaded by Angular Router.
+  // Contains info about the route, its parameters, and additional data associated with the route.
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // subscribe to route params and get the product based on the productId
+    // route parameters correspond to the path variables defined in the route.
+    // The productId is provided by the URL.
+    // Use productId to display the details for each product.
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+    });
   }
 
 }
